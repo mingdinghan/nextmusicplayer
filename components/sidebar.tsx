2e026f5a6f3627f0bba6a29502b28288639bd42f
Mingding Han <mingdinghan@gmail.com>
2022-05-06 23:40:27 +0800

    Fetch and Render Data into Components

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,6 +19,7 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md"
+import { usePlaylist } from '../lib/hooks'
 
 const navMenu = [
   {
@@ -51,9 +52,8 @@ const musicMenu = [
   },
 ]
 
-const playLists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
-
 const Sidebar = () => {
+  const { playlists } = usePlaylist()
   return (
     <Box
       width="100%"
@@ -109,11 +109,11 @@ const Sidebar = () => {
         <Divider color="gray.800"/>
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
-            {playLists.map(playlist => (
-              <ListItem paddingX="20px" key={playlist} >
+            {playlists.map(playlist => (
+              <ListItem paddingX="20px" key={playlist.id} >
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
