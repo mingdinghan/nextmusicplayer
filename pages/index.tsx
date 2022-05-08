6d6043ff17cc23ab07b6b1a985bf3de814c44ea1
Mingding Han <mingdinghan@gmail.com>
2022-05-08 22:39:59 +0800

    Add Artist List UI Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Image, Text, Flex } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
 import prisma from '../lib/prisma'
 
@@ -5,13 +6,36 @@ const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage
-      color="red"
+      color="purple"
       subtitle="profile"
       title="Bluey"
       description="15 public playlists"
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png"
     >
-      <div>home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="http://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
 }
