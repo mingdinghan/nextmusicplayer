a7e0365be97e5d994ae9dff3dd0ee65dfed7a9d1
Mingding Han <mingdinghan@gmail.com>
2022-05-09 23:30:51 +0800

    Update Player UI container and store player state

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/layout";
 import Sidebar from "./sidebar";
-// Box is like a <div> but can be styled
+import PlayerBar from "./playerBar"
 
 const PlayerLayout = ({ children }) => {
   return (
@@ -12,7 +12,7 @@ const PlayerLayout = ({ children }) => {
         <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
       <Box position="absolute" left="0" bottom="0">
-        Player
+        <PlayerBar />
       </Box>
     </Box>
   );
